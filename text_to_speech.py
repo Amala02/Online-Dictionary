d70135d782aad54390ac0e8eb90dd79eed2f9d2f
Amala Shwetah <93658002+Amala02@users.noreply.github.com>
2022-10-09 22:10:09 +0530

    Add files via upload

---
+++ text_to_speech.py
@@ -0,0 +1,54 @@
+from tkinter import *
+import tkinter as tk
+from gtts import gTTS
+from playsound import playsound
+import requests
+import json
+
+root = Tk()
+root.geometry('600x600')
+root.resizable(0,0)
+root.config(bg= '#73cef0')
+root.title('Text_to_speech')
+
+
+Label(root, text= 'TEXT-TO-SPEECH', font = 'Poppins 20 italic',  foreground="white", background="black", width= '20').pack(side='top')
+Msg= StringVar()
+Label(root, text= "enter the text here", font = 'Open-sans 15 italic underline').place(x=20, y=60)
+searchbar= Entry(root, textvariable= 'Msg', width='50')
+searchbar.place(x=20, y=100)
+
+
+
+
+def define():
+    word= searchbar.get()
+    print(word)
+    req= requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")
+    ans=req.json()
+    ans= ans[0]
+    ans= ans['meanings']
+    ans=ans[0]
+    print(ans)
+    #meaning display
+    T= Text(root, height=20, width= 50)
+    T.insert(END, ans)
+    T.place(x=25, y=300)
+    
+def Text_To_Speech():
+    Message= searchbar.get()
+    speech= gTTS(text= Message)
+    speech.save('Speech.mp3')
+    playsound('Speech.mp3')
+
+def Exit():
+    root.destroy()
+
+def Reset():
+    Msg.set("")
+
+Button(root, text= "Hear", font = 'arial 15 bold', command=Text_To_Speech, width =4).place(x=25, y=140)
+Button(root, text= "Exit", font= 'arial 15 bold',  command= Exit, width= 4).place(x=100, y=140)
+Button(root, text= "Reset", font = 'arial 15 bold', command=Reset, width =4).place(x=175, y=140)
+Button(root, text="Definition", font ='arial 15 bold', command= define, width =10).place(x=25, y= 200)
+root.mainloop()
